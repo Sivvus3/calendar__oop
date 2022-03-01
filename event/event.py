ef9084150e0e38a0f25270e02c02532344fe10d2
Sivvus3 <artur.siwak6@gmail.com>
2022-03-01 15:34:15 +0100

    example of polymorphism

--- event/event.py
+++ event/event.py
@@ -10,6 +10,9 @@ class Event:
         self.owner = owner
         self.participants = participants
 
+    def end_of_meeting(self):
+        return self.start_date + timedelta(minutes=self.duration)
+
     @property
     def location(self):
         return self._location
--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from planner import Planner
 
 
 def main():
-    e = Event('python', datetime.strptime('01-03-2022 16:19', '%d-%m-%Y %H:%M'), 6, 'better world', 'pawel',
+    e = Event('python', datetime.strptime('01-03-2022 16:19', '%d-%m-%Y %H:%M'), 60, 'better world', 'pawel',
               ('michal', 'marcin', 'renata'))
     w = Workshop('python', datetime.strptime('01-03-2022 16:19', '%d-%m-%Y %H:%M'), 60, 'better world',
                  'pawel', ['michal', 'marcin', 'renata'], 'online')
     m = Meeting('ala ma ejc', datetime.strptime('01-03-2022 16:19', '%d-%m-%Y %H:%M'), 60, 'better world',
                 'pawel', ['michal', 'marcin', 'renata'], 'arctovsky')
-    e.start_date = datetime.strptime('01-04-2022 16:19', '%d-%m-%Y %H:%M')
-    print(repr(e))
+    e.start_date = datetime.strptime('01-03-2022 16:19', '%d-%m-%Y %H:%M')
 
+    print(w.duration)
+    print(w.start_date)
+    print(w.end_of_meeting())
+    print(w.end_of_meeting() - e.start_date)
     # print(repr(e))
     # print(repr(w))
     # print(repr(m))
