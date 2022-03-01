3c71a0cf898c53df2802e156d7e405189a420abd
Sivvus3 <artur.siwak6@gmail.com>
2022-03-01 17:12:30 +0100

    calendar done

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
+import os
 from datetime import datetime
+
+from data_generator.generator import Generator
 from event import Workshop, Meeting, Event
 from planner import Planner
 
 
 def main():
-    e = Event('python', datetime.strptime('01-03-2022 16:19', '%d-%m-%Y %H:%M'), 60, 'better world', 'pawel',
-              ('michal', 'marcin', 'renata'))
-    w = Workshop('python', datetime.strptime('01-03-2022 16:19', '%d-%m-%Y %H:%M'), 60, 'better world',
-                 'pawel', ['michal', 'marcin', 'renata'], 'online')
-    m = Meeting('ala ma ejc', datetime.strptime('01-03-2022 16:19', '%d-%m-%Y %H:%M'), 60, 'better world',
-                'pawel', ['michal', 'marcin', 'renata'], 'arctovsky')
-    e.start_date = datetime.strptime('01-03-2022 16:19', '%d-%m-%Y %H:%M')
+    data = Generator.load_data(os.path.join('data_generator', 'data.txt'))
+    event_list = []
 
-    print(w.duration)
-    print(w.start_date)
-    print(w.end_of_meeting())
-    print(w.end_of_meeting() - e.start_date)
-    # print(repr(e))
-    # print(repr(w))
-    # print(repr(m))
-    # check_dt = e.check_date(datetime.strptime('01-03-2022 16:19', '%d-%m-%Y %H:%M'))
-    # print(check_dt)
+    for item in data:
+        element_list = []
+        participant_list = []
+        for idx, element in enumerate(item.split(', ')):
+            if element.isdigit():
+                element_list.append(int(element))
+                continue
 
-    # planner = Planner([])
-    # print(repr(planner))
+            try:
+                dt = datetime.strptime(element, '%Y-%m-%d %H:%M:%S')
+                element_list.append(dt)
+                continue
+            except Exception as err:
+                pass
+
+            if idx >= 5:
+                participant_list.append(element)
+                continue
+
+            element_list.append(element)
+
+        element_list.append(participant_list)
+        event_list.append(Event(*element_list))
+
+    planner = Planner(event_list)
+    print(repr(planner))
 
 
 if __name__ == '__main__':
